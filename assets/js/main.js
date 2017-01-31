fcec044cafebf811aed66426b0a059172b26827f
Brent Brinkley <brentbrinkley@gmail.com>
2017-01-31 17:03:46 -0500

    Changes to make navbar stick

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
-$(window).scroll(function(){
-
-    console.log("loaded")
+$(document).ready( function(){
     
-    var wScroll = $(this).scrollTop();
+    // variable inits
+    let navOffset; //offsetTop of the nav wrapper
 
-     $('.background').css({
-        'transform' : 'translate(0px,-' + wScroll / 80 + '%)' 
+    function setOffset(){ //calculate offset of nav wrapper and set navOffset
+        navOffset = $('.navwrap').offset().top;
+    }
+
+    setOffset(); //run on document load
+
+    $(window).resize( function(){
+        setOffset(); //run again on every resize event
     });
+
+$(window).scroll(function(){
+    
+    var wScroll = $(this).scrollTop(); // check scroll position
+
+     /*$('.background').css({
+        'transform' : 'translate(0px,-' + wScroll / 80 + '%)' 
+    });*/
 
     $('.card').css({
         'transform' : 'translate(0px,' + wScroll / 6 + '%)' 
@@ -14,14 +27,15 @@ $(window).scroll(function(){
 
     $(".card").css("opacity", 1.2 - wScroll / 300);
 
-    
-    navOffset = $('.navwrap').offset().top;
-    console.log(navOffset)
-
-    if(wScroll > navOffset){
-        // $( ".navwrap" ).removeClass( "navwrap" ).addClass( "fixed-nav" );
-        console.log('success is delicious');
+    if(wScroll >= navOffset){
+        // if window scroll is equal or greater than navoffset, add fixed class to all the things
+        $('.background, .card-wrapper, .nav-wrap, body').addClass('is-fixed');
+    }else{
+        // otherwise remove it
+        $('.background, .card-wrapper, .nav-wrap, body').removeClass('is-fixed');
     }
     
     
+});
+
 });
